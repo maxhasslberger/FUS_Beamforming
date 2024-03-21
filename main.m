3d06f3c2f5b78a1b83b641612cafe3d092a3c30a
Max Hasslberger <max.hasslberger@tum.de>
2024-03-21 11:40:01 -0700

    Separating Multi foci from Holography IP optimisation

--- main.m
+++ main.m
@@ -10,24 +10,32 @@ dx_factor = 1;
 sensor = init_sensor(kgrid, ppp);
 sensor_plane = ones(kgrid.Nx, kgrid.Ny);
 
+only_focus_opt = true; % Optimize only focal spots or entire grid
+
 %% Define Transducer Geometry
 
 % karray_t = kWaveArray();
-
-% % Annular
-% karray.addAnnularArray(bowl_pos, source_roc, diameters, focus_pos);
 
 el1_offset = round(0.05 * kgrid.Nx); % grid points
 el2_offset = round(0.05 * kgrid.Ny); % grid points
 shift = 0; % grid points -> tangential shift
 
 % Linear array
-num_elements = 42;
-spacing = ceil(2 * dx_factor); % grid points between elements
-t_mask = create_linear_array(kgrid, num_elements, el1_offset, shift, spacing, false);
-t_mask = t_mask + create_linear_array(kgrid, num_elements, el2_offset, shift, spacing, true); % Second (orthogonal) linear array
-% t_mask = t_mask + create_linear_array(kgrid, num_elements, round(kgrid.Nx - el1_offset), shift, spacing, false); % Second (antiparallel) linear array
-% t_mask = t_mask + create_linear_array(kgrid, num_elements, round(kgrid.Nx - el2_offset), shift, spacing, true); % Third (orthogonal) linear array
+if only_focus_opt
+    num_elements = 84;
+    spacing = ceil(1 * dx_factor); % grid points between elements
+    t_mask = create_linear_array(kgrid, num_elements, el1_offset, shift, spacing, false);
+    % t_mask = t_mask + create_linear_array(kgrid, num_elements, el2_offset, shift, spacing, true); % Second (orthogonal) linear array
+    % t_mask = t_mask + create_linear_array(kgrid, num_elements, round(kgrid.Nx - el1_offset), shift, spacing, false); % Second (antiparallel) linear array
+    % t_mask = t_mask + create_linear_array(kgrid, num_elements, round(kgrid.Nx - el2_offset), shift, spacing, true); % Third (orthogonal) linear array
+else
+    num_elements = 42;
+    spacing = ceil(2 * dx_factor); % grid points between elements
+    t_mask = create_linear_array(kgrid, num_elements, el1_offset, shift, spacing, false);
+    t_mask = t_mask + create_linear_array(kgrid, num_elements, el2_offset, shift, spacing, true); % Second (orthogonal) linear array
+    % t_mask = t_mask + create_linear_array(kgrid, num_elements, round(kgrid.Nx - el1_offset), shift, spacing, false); % Second (antiparallel) linear array
+    % t_mask = t_mask + create_linear_array(kgrid, num_elements, round(kgrid.Nx - el2_offset), shift, spacing, true); % Third (orthogonal) linear array
+end
 
 % % Curvature
 % grid_size = [kgrid.Nx, kgrid.Ny];
@@ -48,27 +56,32 @@ t_mask = t_mask > 0; % Return to logical in case of overlaps
 %% Define (intracranial) Beamforming Pattern
 
 if kgrid.dim == 2
+    
+    if ~only_focus_opt
 
-    % Ring
-    b_mask = makeDisc(kgrid.Nx, kgrid.Ny, kgrid.Nx/2, kgrid.Ny/2, round(0.2 * kgrid.Nx), false) ...
-        - makeDisc(kgrid.Nx, kgrid.Ny, kgrid.Nx/2, kgrid.Ny/2, round(0.15 * kgrid.Nx), false);
-    amp = 30000 * ones(sum(b_mask(:)), 1);
+        % Ring
+        b_mask = makeDisc(kgrid.Nx, kgrid.Ny, kgrid.Nx/2, kgrid.Ny/2, round(0.2 * kgrid.Nx), false) ...
+            - makeDisc(kgrid.Nx, kgrid.Ny, kgrid.Nx/2, kgrid.Ny/2, round(0.15 * kgrid.Nx), false);
+        amp = 30000 * ones(sum(b_mask(:)), 1);
+    else
 
-%     % Points
-%     point_posx = round([0.5, 0.2, 0.8] * kgrid.Nx);
-%     point_posy = round([0.6, 0.5, 0.2] * kgrid.Ny);
-%     amp = [10, 10, 10]' * 1e3;
-% %     amp = 30000 * ones(length(point_posx), 1);
-% 
-%     % Assign amplitude acc. to closest position
-%     idx = sub2ind([kgrid.Nx, kgrid.Ny], point_posx, point_posy);
-%     [~, order] = sort(idx);
-%     amp = amp(order);
-% 
-%     b_mask = zeros(kgrid.Nx, kgrid.Ny);
-%     for point = 1:length(point_posx)
-%         b_mask(point_posx(point), point_posy(point)) = 1;
-%     end
+        % Points
+        point_posx = round([0.5, 0.2, 0.8] * kgrid.Nx);
+        point_posy = round([0.6, 0.5, 0.2] * kgrid.Ny);
+        amp = [10, 10, 10]' * 1e3;
+%         amp = 30000 * ones(length(point_posx), 1);
+    
+        % Assign amplitude acc. to closest position
+        idx = sub2ind([kgrid.Nx, kgrid.Ny], point_posx, point_posy);
+        [~, order] = sort(idx);
+        amp = amp(order);
+    
+        b_mask = zeros(kgrid.Nx, kgrid.Ny);
+
+        for point = 1:length(point_posx)
+            b_mask(point_posx(point), point_posy(point)) = 1;
+        end
+    end
 else
     error("Not supported at the moment")
 end
@@ -85,7 +98,7 @@ imagesc(b_mask + t_mask, [-1 1])
 colormap(getColorMap);
 
 % set simulation input options
-input_args = {'PMLSize', 'auto', 'PMLInside', false, 'PlotPML', true, 'DisplayMask', b_mask + t_mask, 'RecordMovie', true};
+input_args = {'PMLSize', 'auto', 'PMLInside', false, 'PlotPML', true, 'DisplayMask', b_mask + t_mask, 'RecordMovie', false};
 
 %% Time Reversal
 
@@ -97,18 +110,21 @@ b_tr = sim_exe(kgrid, medium, sensor, f0, p_tr, t_mask, sensor_plane, true, inpu
 
 % Obtain propagation operator
 % A = linearPropagator_vs_acousticFieldPropagator(t_mask, f0, medium.sound_speed, kgrid.dx);
-A = obtain_linear_propagator(t_mask, b_mask, f0, medium.sound_speed, kgrid.dx); % acousticFieldPropagator (Green's functions)
+A = obtain_linear_propagator(t_mask, b_mask, f0, medium.sound_speed, kgrid.dx, only_focus_opt); % acousticFieldPropagator (Green's functions)
 
 % Solve inverse problem
 tic
+if only_focus_opt
+    p_ip = pinv(A) * b_des;
+else
 
-% p_ip = pinv(A) * b_des;
-p_ip = pinv(A) * b_des_pl;
-
-opts = struct;
-opts.initMethod = 'custom';
-opts.customx0 = p_ip;
-[p_ip, outs, opts] = solvePhaseRetrieval(A, A', b_des_pl, [], opts);
+    p_ip = pinv(A) * b_des_pl;
+    
+    opts = struct;
+    opts.initMethod = 'custom';
+    opts.customx0 = p_ip;
+    [p_ip, outs, opts] = solvePhaseRetrieval(A, A', b_des_pl, [], opts);
+end
 
 t_solve = toc;
 
@@ -121,17 +137,23 @@ plot_results(kgrid, p_ip, b_ip, 'Inverse Problem');
 
 disp("Time until solver converged: " + string(t_solve) + " s")
 
-% b_tr_points = [];
-% b_ip_points = [];
-% for point = 1:length(point_posx)
-%     b_tr_points = [b_tr_points, b_tr(point_posx(point), point_posy(point))];
-%     b_ip_points = [b_ip_points, b_ip(point_posx(point), point_posy(point))];
-% end
-% 
-% amp
-% abs(b_tr_points)
-% abs(b_ip_points)
-% angle(b_tr_points)
-% angle(b_ip_points)
-
+if only_focus_opt
+    b_tr_points = [];
+    b_ip_points = [];
+    for point = 1:length(point_posx)
+        b_tr_points = [b_tr_points, b_tr(point_posx(point), point_posy(point))];
+        b_ip_points = [b_ip_points, b_ip(point_posx(point), point_posy(point))];
+    end
+    
+    fprintf("\nInput Amplitudes (kPa):\n")
+    disp(amp' * 1e-3)
+    fprintf("\nTime Reversal Total Amplitudes (kPa):\n")
+    disp(abs(b_tr_points) * 1e-3)
+    fprintf("\nInverse Problem Total Amplitudes (kPa):\n")
+    disp(abs(b_ip_points) * 1e-3)
+    fprintf("\nTime Reversal Phase Angles (kPa):\n")
+    disp(angle(b_tr_points) * 1e-3)
+    fprintf("\nInverse Problem Phase Angles (kPa):\n")
+    disp(angle(b_ip_points) * 1e-3)
+end
 
--- obtain_linear_propagator.m
+++ obtain_linear_propagator.m
@@ -1,12 +1,16 @@
-function A = obtain_linear_propagator(t_mask, b_mask, f0, sound_speed, dx)
+function A = obtain_linear_propagator(t_mask, b_mask, f0, sound_speed, dx, only_focus_opt)
 
 if min(sound_speed(:)) == max(sound_speed(:))
 
     % Excite one element after the other
     phase_in = 0;
     el_ids = find(t_mask);
-    obs_ids = find(b_mask);
-    obs_ids = 1:numel(t_mask);
+    
+    if only_focus_opt
+        obs_ids = find(b_mask);
+    else
+        obs_ids = 1:numel(t_mask);
+    end
     
     A = zeros(numel(obs_ids), numel(el_ids));
     for i = 1:length(el_ids)
