9563e878cfbd20364c32896a27df933365d46582
Max Hasslberger <max.hasslberger@tum.de>
2024-03-23 15:36:32 -0700

    Place and align multiple transducers on the grid

--- create_transducer.m
+++ create_transducer.m
@@ -5,24 +5,35 @@ n_dim = 3;
 dx_factor = 1;
 [kgrid, medium, ppp] = init_grid_medium(f0, 'n_dim', n_dim, 'dx_factor', 1 / dx_factor);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Init
-karray_t = kWaveArray();
-element_pos = load(fullfile('Array_Positions\std.mat')).ElementPosition';
+element_pos = load(fullfile('Array_Positions\std.mat')).ElementPosition'; % flat transducer array centered along [0, 0, 0] on the xy-plane
+t_pos = [-45, 0; 0, -45; 0, 0] * 1e-3; % m
+t_rot = [0, 90; 90, 0; 0, 0]; % deg
 
-% Add one array element after another
-for i = 1:length(element_pos)
-%     karray_t.addCustomElement(round(element_pos(:, i), 3), 0, 2, char("el_" + string(i)));
-    karray_t.addDiscElement(round(element_pos(:, i), 3), 1e-10, ones(1, 3));
-%     mask = karray_t.getArrayBinaryMask(kgrid);
-%     voxelPlot(double(mask))
+karray_t = kWaveArray();
+
+for tx = 1:size(t_pos, 2) % for each transducer
+
+    tx_pos = t_pos(:, tx);
+    tx_rot = t_rot(:, tx);
+
+    % Shift and align transducers 
+    tx_transf = getAffineMatrix(tx_pos, tx_rot);
+    
+    elementx_pos = tx_transf(1:3, 1:3) * element_pos;
+    elementx_pos = elementx_pos + repmat(tx_pos, 1, size(element_pos, 2));
+    
+    % Add one array element after another
+    for i = 1:length(elementx_pos)
+%         karray_t.addCustomElement(round(element_pos(:, i), 3), 0, 2, char("el_" + string(i)));
+        karray_t.addDiscElement(round(elementx_pos(:, i), 3), 1e-10, ones(1, 3)); % Rounding error can introduce error in spacing!
+%         mask = karray_t.getArrayBinaryMask(kgrid);
+%         voxelPlot(double(mask))
+    end
 end
 
 % TODO: Rotation and translation considered before added to karray class
-
-% % Translation and Rotation of every single element...
-% translation = [0, 0, 0] * 1e-3;
-% rotation = [0, 90, 0]; % only integers of 90° supported
-% karray_t.setArrayPosition(translation, rotation);
 
 mask = karray_t.getArrayBinaryMask(kgrid);
 voxelPlot(double(mask))
