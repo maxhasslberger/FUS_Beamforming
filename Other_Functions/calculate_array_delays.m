902d9daa7a8d14d81faefdfcd232d9d72841ba52
Max Hasslberger <max.hasslberger@tum.de>
2024-03-25 16:49:41 -0700

    Save results in mat file

--- Other_Functions/calculate_array_delays.m
+++ Other_Functions/calculate_array_delays.m
@@ -1,14 +1,14 @@
 clear;
 
 %% Load desired Results file
-in_filename = "res";
-load(fullfile("..", in_filename + ".mat"));
+in_filename = "20240325164602_results";
+load(fullfile("..", "Results", in_filename + ".mat"));
 
 %% Compute Delays for Time Reversal and Inverse Problem
-tr_angles = angle(tr.p_tr') + pi; % between 0 and 2*pi
+tr_angles = angle(tr.p') + pi; % between 0 and 2*pi
 tr_delays = tr_angles / (2*pi * f0);
 
-ip_angles = angle(ip.p_ip') + pi; % between 0 and 2*pi
+ip_angles = angle(ip.p') + pi; % between 0 and 2*pi
 ip_delays = ip_angles / (2*pi * f0);
 
 %% Save as txt files
--- main.m
+++ main.m
@@ -16,6 +16,7 @@ end
 
 only_focus_opt = true; % Optimize only focal spots or entire grid
 set_current_A = false; % Use precomputed propagation matrix - can be logical or a string containing the file name in Lin_Prop_Matrices
+save_results = true;
 
 %% Define Transducer Geometry
 
@@ -76,25 +77,25 @@ if kgrid.dim == 2
         % Ring
         b_mask = makeDisc(kgrid.Nx, kgrid.Ny, round(0.7 * kgrid.Nx), round(0.7 * kgrid.Ny), round(0.2 * kgrid.Nx), false) ...
             - makeDisc(kgrid.Nx, kgrid.Ny, round(0.7 * kgrid.Nx), round(0.7 * kgrid.Ny), round(0.15 * kgrid.Nx), false);
-        amp = 30000 * ones(sum(b_mask(:)), 1);
+        amp_in = 30000 * ones(sum(b_mask(:)), 1);
     else
 
         % Points
-        point_posx = round([0.2, 0.5, 0.6] * kgrid.Nx);
-        point_posy = round([0.5, 0.6, 0.3] * kgrid.Ny);
-        point_posz = [];
-        amp = [0, 100, 200]' * 1e3;
+        point_pos.x = round([0.2, 0.5, 0.6] * kgrid.Nx);
+        point_pos.y = round([0.5, 0.6, 0.3] * kgrid.Ny);
+        point_pos.z = [];
+        amp_in = [0, 100, 200]' * 1e3;
 %         amp = 30000 * ones(length(point_posx), 1);
     
         % Assign amplitude acc. to closest position
-        idx = sub2ind([kgrid.Nx, kgrid.Ny], point_posx, point_posy);
+        idx = sub2ind([kgrid.Nx, kgrid.Ny], point_pos.x, point_pos.y);
         [~, order] = sort(idx);
-        amp = amp(order);
+        amp_in = amp_in(order);
     
         b_mask = zeros(kgrid.Nx, kgrid.Ny);
 
-        for point = 1:length(point_posx)
-            b_mask(point_posx(point), point_posy(point)) = 1;
+        for point = 1:length(point_pos.x)
+            b_mask(point_pos.x(point), point_pos.y(point)) = 1;
         end
     end
 
@@ -104,29 +105,29 @@ else
     only_focus_opt = true;
 
     % Points
-    point_posx = round([0.2, 0.7, 0.8] * kgrid.Nx);
-    point_posy = round([0.5, 0.8, 0.5] * kgrid.Ny);
-    point_posz = round([0.5, 0.5, 0.5] * kgrid.Nz);
-    amp = [10, 10, 10]' * 1e3;
+    point_pos.x = round([0.2, 0.7, 0.8] * kgrid.Nx);
+    point_pos.y = round([0.5, 0.8, 0.5] * kgrid.Ny);
+    point_pos.z = round([0.5, 0.5, 0.5] * kgrid.Nz);
+    amp_in = [10, 10, 10]' * 1e3;
 
     % Assign amplitude acc. to closest position
-    idx = sub2ind([kgrid.Nx, kgrid.Ny, kgrid.Nz], point_posx, point_posy, point_posz);
+    idx = sub2ind([kgrid.Nx, kgrid.Ny, kgrid.Nz], point_pos.x, point_pos.y, point_pos.z);
     [~, order] = sort(idx);
-    amp = amp(order);
+    amp_in = amp_in(order);
 
     b_mask = zeros(kgrid.Nx, kgrid.Ny, kgrid.Nz);
 
-    for point = 1:length(point_posx)
-        b_mask(point_posx(point), point_posy(point), point_posz(point)) = 1;
+    for point = 1:length(point_pos.x)
+        b_mask(point_pos.x(point), point_pos.y(point), point_pos.z(point)) = 1;
     end
 
     voxelPlot(double(t_mask + b_mask))
 end
 
 % Create desired signal
-phase = zeros(length(amp), 1); % Zero phase for entire observation plane
+phase = zeros(length(amp_in), 1); % Zero phase for entire observation plane
 
-b_des = amp .* exp(1j*phase); % only observed elements
+b_des = amp_in .* exp(1j*phase); % only observed elements
 
 b_des_pl = zeros(kgrid.Nx * kgrid.Ny, 1); % entire plane
 b_des_pl(find(b_mask)) = b_des;
@@ -137,77 +138,63 @@ input_args = {'PMLSize', 'auto', 'PMLInside', false, 'PlotPML', true, 'DisplayMa
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Time Reversal
 
-p_tr = sim_exe(kgrid, medium, sensor, f0, b_des, b_mask, t_mask, false, input_args);
-p_tr = max(abs(p_tr)) * exp(-1j * angle(p_tr)); % All elements with same amplitude
-b_tr = sim_exe(kgrid, medium, sensor, f0, p_tr, t_mask, sensor_mask, true, input_args, 'karray_t', karray_t);
+tr.p = sim_exe(kgrid, medium, sensor, f0, b_des, b_mask, t_mask, false, input_args);
+tr.p = max(abs(tr.p)) * exp(-1j * angle(tr.p)); % All elements with same amplitude
+tr.b = sim_exe(kgrid, medium, sensor, f0, tr.p, t_mask, sensor_mask, true, input_args, 'karray_t', karray_t);
 
 %% Inverse Problem
 
 % Obtain propagation operator
 % A = linearPropagator_vs_acousticFieldPropagator(t_mask, f0, medium.sound_speed, kgrid.dx);
-A = obtain_linear_propagator(t_mask, b_mask, f0, medium.sound_speed, kgrid.dx, only_focus_opt, set_current_A); % -> acousticFieldPropagator (Green's functions)
+ip.A = obtain_linear_propagator(t_mask, b_mask, f0, medium.sound_speed, kgrid.dx, only_focus_opt, set_current_A); % -> acousticFieldPropagator (Green's functions)
 
 % Solve inverse problem
 tic
 if only_focus_opt
     b_ip_des = b_des;
-    sq_beta = 0;%0.1; % constraint scaling factor
+    ip.sq_beta = 0;%0.1; % constraint scaling factor
 else
     b_ip_des = b_des_pl;
-    sq_beta = 0;%100; % constraint scaling factor
+    ip.sq_beta = 0;%100; % constraint scaling factor
 end
 
-p_ip = pinv(A) * b_ip_des; % Initial solution cosidering phases
+ip.p = pinv(ip.A) * b_ip_des; % Initial solution cosidering phases
 
-u = max(abs(p_ip)) * ones(size(p_ip)); % universal transducer amplitude
+ip.u = max(abs(ip.p)) * ones(size(ip.p)); % universal transducer amplitude
 
 % Apply constraints (same transducer amplitude among elements)
-A = [A; sq_beta * eye(length(p_ip))];
-b_ip_des = [b_ip_des; sq_beta * u];
+ip.A = [ip.A; ip.sq_beta * eye(length(ip.p))];
+b_ip_des = [b_ip_des; ip.sq_beta * ip.u];
 
 % Solve phase retrieval problem
 opts = struct;
 opts.initMethod = 'custom';
-opts.customx0 = p_ip;
-[p_ip, outs, opts] = solvePhaseRetrieval(A, A', b_ip_des, [], opts);
+opts.customx0 = ip.p;
+[ip.p, outs, opts] = solvePhaseRetrieval(ip.A, ip.A', b_ip_des, [], opts);
 
-t_solve = toc;
+ip.t_solve = toc;
 
 % Evaluate obtained phase terms in forward simulation
-b_ip = sim_exe(kgrid, medium, sensor, f0, p_ip, t_mask, sensor_mask, true, input_args, 'karray_t', karray_t);
+ip.b = sim_exe(kgrid, medium, sensor, f0, ip.p, t_mask, sensor_mask, true, input_args, 'karray_t', karray_t);
 
 %% Save Results in mat-file
-% f0
-% kgrid
-% b_mask
-% t_mask
-% t1_pos
-% t2_pos
-
-tr.p_tr = p_tr;
-tr.b_tr = b_tr;
-
-ip.A = A;
-ip.p_ip = p_ip;
-ip.b_ip = b_ip;
-ip.t_solve = t_solve;
-ip.u = u;
-ip.sq_beta = sq_beta;
-
-point_pos.x = point_posx;
-point_pos.y = point_posy;
-point_pos.z = point_posz;
+if save_results
+    current_datetime = string(datestr(now, 'yyyymmddHHMMSS'));
+    res_filename = "results";
+    save(fullfile("Results", current_datetime + "_" + res_filename + ".mat"), ...
+        "f0", "kgrid", "b_mask", "t_mask", "t1_pos", "t2_pos", "tr", "ip", "amp_in", "point_pos", "only_focus_opt");
+end
 
 %% Results
-plot_results(kgrid, p_tr, b_tr, 'Time Reversal');
-plot_results(kgrid, p_ip, b_ip, 'Inverse Problem');
+plot_results(kgrid, tr.p, tr.b, 'Time Reversal');
+plot_results(kgrid, ip.p, ip.b, 'Inverse Problem');
 
 % Metrics evaluation
-disp("Time until solver converged: " + string(t_solve) + " s")
+disp("Time until solver converged: " + string(ip.t_solve) + " s")
 fprintf("\nDesired Transducer Amplitude (kPa):\n")
-disp(u(1) * 1e-3)
+disp(ip.u(1) * 1e-3)
 fprintf("\nTransducer Amplitude mean deviation (Pa):\n")
-disp(mean(abs(abs(p_ip) - u)))
+disp(mean(abs(abs(ip.p) - ip.u)))
 
 % Evaluation of each defined point
 if only_focus_opt
@@ -215,19 +202,19 @@ if only_focus_opt
     b_ip_points = [];
 
     if kgrid.dim == 2
-        for point = 1:length(point_posx)
-            b_tr_points = [b_tr_points, b_tr(point_posx(point), point_posy(point))];
-            b_ip_points = [b_ip_points, b_ip(point_posx(point), point_posy(point))];
+        for point = 1:length(point_pos.x)
+            b_tr_points = [b_tr_points, tr.b(point_pos.x(point), point_pos.y(point))];
+            b_ip_points = [b_ip_points, ip.b(point_pos.x(point), point_pos.y(point))];
         end
     else
-        for point = 1:length(point_posx)
-            b_tr_points = [b_tr_points, b_tr(point_posx(point), point_posy(point), point_posz(point))];
-            b_ip_points = [b_ip_points, b_ip(point_posx(point), point_posy(point), point_posz(point))];
+        for point = 1:length(point_pos.x)
+            b_tr_points = [b_tr_points, tr.b(point_pos.x(point), point_pos.y(point), point_pos.z(point))];
+            b_ip_points = [b_ip_points, b_ip(point_pos.x(point), point_pos.y(point), point_pos.z(point))];
         end
     end
     
     fprintf("\nInput Amplitudes (kPa):\n")
-    disp(amp' * 1e-3)
+    disp(amp_in' * 1e-3)
     fprintf("\nTime Reversal Total Amplitudes (kPa):\n")
     disp(abs(b_tr_points) * 1e-3)
     fprintf("\nInverse Problem Total Amplitudes (kPa):\n")
