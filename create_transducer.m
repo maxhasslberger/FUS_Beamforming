45f3f8e80a4777c162bb376671f4caca853ee96c
Max Hasslberger <max.hasslberger@tum.de>
2024-03-28 22:53:21 -0700

    Correct indexing confirmed

--- create_transducer.m
+++ create_transducer.m
@@ -3,9 +3,9 @@ function [karray_t, el2mask_ids] = create_transducer(kgrid, t_name, t_pos, t_rot
 % Init
 dim = 2;
 element_pos = load(fullfile("Array_Positions", t_name + ".mat")).ElementPosition'; % flat transducer array centered at [0, 0, 0] along the xy-plane
-% element_pos = element_pos(1:dim, [1, 13, 26, 39, 42, 55, 67, 140, 210])%2D
+element_pos = element_pos(1:dim, [1, 13, 26, 39, 42, 55, 67, 140, 210])%2D
 % element_pos = element_pos(1:dim, [1, 67, 140, 210])%2D
-element_pos = element_pos(1:dim, 200:256);%2D
+% element_pos = element_pos(1:dim, 200:256);%2D
 karray_t = kWaveArray();
 
 n_tr_elements = size(element_pos, 2);
@@ -25,7 +25,7 @@ end
 [elementAll_pos, el2mask_ids] = sortrows(elementAll_pos_orig', 2); % refer element indices to mask -> getDistributedSourceSignal
 elementAll_pos = elementAll_pos'; % TODO: Fix indexing!
 % elementAll_pos = elementAll_pos_orig;
-elementAll_pos;
+elementAll_pos
 
 % TODO: Real mask with all elements (karray) + mask with center elements only
     
@@ -36,13 +36,13 @@ for i = 1:length(elementAll_pos)
 %     mask = karray_t.getArrayBinaryMask(kgrid);
 %     voxelPlot(double(mask))
 end
-mask = karray_t.getArrayBinaryMask(kgrid);
-% voxelPlot(double(mask))
-imagesc(mask, [-1 1])
-colormap(getColorMap);
-
-karr2 = kWaveArray();
-karr2.addDiscElement(round(elementAll_pos(:, 1), 3), 1e-4, zeros(1, 3));
-mask2 = karr2.getArrayBinaryMask(kgrid);
+% mask = karray_t.getArrayBinaryMask(kgrid);
+% % voxelPlot(double(mask))
+% imagesc(mask, [-1 1])
+% colormap(getColorMap);
+% 
+% karr2 = kWaveArray();
+% karr2.addDiscElement(round(elementAll_pos(:, 1), 3), 1e-4, zeros(1, 3));
+% mask2 = karr2.getArrayBinaryMask(kgrid);
 
 end
