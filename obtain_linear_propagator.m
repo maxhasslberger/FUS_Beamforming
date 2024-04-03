b61145a42f7232ac0df1bae4fcebc1d3a9281f46
Max Hasslberger <max.hasslberger@tum.de>
2024-04-03 10:31:08 -0700

    Only use one array element at at time to obtain A

--- obtain_linear_propagator.m
+++ obtain_linear_propagator.m
@@ -40,18 +40,25 @@ if islogical(get_current_A)
         
         else
             A = single(zeros(kgrid.total_grid_points, karray_t.number_elements));
-            input = zeros(karray_t.number_elements, 1);
-            input(1) = 1.0;
+%             input = zeros(karray_t.number_elements, 1);
+%             input(1) = 1.0;
+
+            input = 1.0;
+            karray_tmp = kWaveArray();
 
             % Excite one element at a time and obtain one column (observation) after the other
             for i = 1:karray_t.number_elements
                 disp("Offline Simulation " + string(i) + "/" + string(karray_t.number_elements))
 
+                el_x = karray_t.elements{i};
+                karray_tmp.addRectElement(el_x.position, el_x.length, el_x.width, el_x.orientation);
+
                 a_coli = single(sim_exe(kgrid, medium, sensor, f0, input, [], sensor_mask, true, input_args, 'karray_t', karray_t));
                 a_coli = reshape(a_coli, [], 1);
                 A(:, i) = a_coli;
 
-                input = circshift(input, 1); % Move unitary excitation by 1 element
+                karray_tmp.removeElement(1);
+%                 input = circshift(input, 1); % Move unitary excitation by 1 element
             end
         end
         
