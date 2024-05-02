bdb6fdfd544c23bf94a88b881ce22f69af1e209d
Max Hasslberger <max.hasslberger@tum.de>
2024-05-01 17:21:07 -0700

    Phi optimization method fixed

--- solvePhasesOnly.m
+++ solvePhasesOnly.m
@@ -14,47 +14,54 @@ else
     A2_i = zeros(1, size(A1, 2));
 end
 
-% Cost fctn and constraints
-fun = @(p)cost_fctn(p, A1_r, A1_i, b1, via_abs);
-nonlcon = @(p)unitdisk(p, A2_r, A2_i, b2, via_abs);
-
-options = optimoptions('fmincon','Display','iter', 'FunctionTolerance', 1e-6, 'ConstraintTolerance', 1e-6, ...
-    'Algorithm','active-set'); % interior-point, sqp, trust-region-reflective, active-set
-
 % Define initial solution
 p_lin = pinv(A1) * b1;
 
 if via_abs
     p_start = zeros(length(p_lin) * 2 + 1, 1);
+    amp_fac = 0.0;
     p_start(1) = mean(abs(p_lin));
 
     p_start(2:ceil(end/2)) = real(p_lin);
     p_start(ceil(end/2) + 1:end) = imag(p_lin); % TODO: Numerical error bc of div by 0?
 else
     p_start = zeros(length(p_lin) + 1, 1);
-    p_start(1) = mean(abs(p_lin));
+    amp_fac = mean(abs(p_lin));
+    p_start(1) = 1.0;
 
     p_start(2:end) = angle(p_lin); % TODO: Numerical precision bc of first element...
 end
 
+% Cost fctn and constraints
+fun = @(p)cost_fctn(p, A1_r, A1_i, b1, via_abs, amp_fac);
+nonlcon = @(p)unitdisk(p, A2_r, A2_i, b2, via_abs);
+
+options = optimoptions('fmincon','Display','iter', 'FunctionTolerance', 1e-6, 'ConstraintTolerance', 1e-6, ...
+    'Algorithm','active-set'); % interior-point, sqp, trust-region-reflective, active-set
+options.MaxFunctionEvaluations = 1e5;
+
+
 [p_opt, fval, exitflag, output] = fmincon(fun, p_start, [], [], [], [], [], [], nonlcon, options);
 
 
-p = p_opt(1) * exp(1j * atan2(p_opt(ceil(end/2) + 1:end), p_opt(2:ceil(end/2))));
+if via_abs
+    p = p_opt(1) * exp(1j * atan2(p_opt(ceil(end/2) + 1:end), p_opt(2:ceil(end/2))));
+else
+    p = p_opt(1) * amp_fac * exp(1j * p_opt(2:end));
+end
 
 end
 
 
-function val = cost_fctn(p, A1_r, A1_i, b1, via_abs)
+function val = cost_fctn(p, A1_r, A1_i, b1, via_abs, amp_fac)
 
 if via_abs
     [x_r, x_i, p_0] = getElements_abs(p);
+    val = sum((p_0 * sqrt((A1_r * x_r - A1_i * x_i).^2 + (A1_i * x_r + A1_r * x_i).^2) - b1).^2);
 else
-    [x_r, x_i, p_0] = getElements_phase(p);
+%     [x_r, x_i, p_0] = getElements_phase(p);
+    val = sum((p(1) * amp_fac * abs((A1_r + 1j * A1_i) * exp(1j*p(2:end))) - b1).^2);
 end
-
-
-val = sum((p_0 * sqrt((A1_r * x_r - A1_i * x_i).^2 + (A1_i * x_r + A1_r * x_i).^2) - b1).^2);
 
 end
 
@@ -70,12 +77,12 @@ x_i = p_i ./ sqrt(p_r.^2 + p_i.^2);
 
 end
 
-function [x_r, x_i, p_0] = getElements_phase(p)
-
-phase_vec = exp(1j * p(2:end));
-p_0 = p(1);
-
-x_r = real(phase_vec);
-x_i = imag(phase_vec);
-
-end
+% function [x_r, x_i, p_0] = getElements_phase(p)
+% 
+% phase_vec = exp(1j * p(2:end));
+% p_0 = p(1);
+% 
+% x_r = real(phase_vec);
+% x_i = imag(phase_vec);
+% 
+% end
