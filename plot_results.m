14499a7546e3d5deea3e11abc3a1e0b3e03502a0
Max Hasslberger <max.hasslberger@tum.de>
2024-05-14 18:04:27 -0700

    White figures

--- plot_results.m
+++ plot_results.m
@@ -24,7 +24,7 @@ end
 if ~isempty(excitation)
     excitation = excitation(reshape(mask2el, 1, [])); % Sort acc to transducer id
     
-    f_param = figure;
+    f_param = figure('color','w');
     f_param.Position = [700 50 650 350];
     subplot(2, 1, 1)
     plot(abs(excitation * 1e-3))
@@ -55,7 +55,7 @@ plot_vecx = linspace(0, grid_size(1)-kgrid.dx, p_sz(1)) / dx_scan;
 plot_vecy = plot_vecy - plot_offset(3) + kgrid.dy / dx_scan;
 plot_vecx = plot_vecx - plot_offset(1) + kgrid.dx / dx_scan;
 
-f_data = figure;
+f_data = figure('color','w');
 f_data.Position = [1400 50 484 512];
 
 % Include a t1w scan
