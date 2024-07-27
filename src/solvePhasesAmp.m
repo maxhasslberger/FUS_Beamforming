e4536fecf861089c9ec123eb70a27b77a100c368
Max Hasslberger <max.hasslberger@tum.de>
2024-07-26 17:11:18 -0700

    Cost fctn into equality constraint

--- src/solvePhasesAmp.m
+++ src/solvePhasesAmp.m
@@ -5,17 +5,20 @@ p_init = double(p_init);
 [A1, A2, b1, b2, ~, ~] = prepare_opt_vars(A, b, domain_ids | skull_ids, vol_ids, init_ids);
 clear A;
 
+A0 = zeros(1, length(p_init));
+b0 = 0.0;
+
 % Add regularization
-[A1, b1] = add_L2_reg(A1, b1, beta(1));
+[A0, b0] = add_L2_reg(A0, b0, beta(1));
 [A2, b2] = add_ineq(A2, b2, length(p_init));
 
 % Cost fctn and constraints
-fun = @(p)cost_fctn(p, A1, b1);
-nonlcon = @(p)ineq_const(p, A2, b2);
-term_fctn = @(x, optimValues, state)customOutputFcn(x, optimValues, state, 1e0, 1e0);
+fun = @(p)cost_fctn(p, A0, b0);
+nonlcon = @(p)ineq_const(p, A1, b1, A2, b2);
+term_fctn = @(x, optimValues, state)customOutputFcn(x, optimValues, state, [], 1e-3);
 
-options = optimoptions('fmincon','Display','iter', 'FunctionTolerance', 1e-6, 'ConstraintTolerance', 1e-6, ...
-    'Algorithm','active-set', 'OutputFcn', term_fctn); % interior-point, sqp, trust-region-reflective, active-set
+options = optimoptions('fmincon','Display','iter', 'FunctionTolerance', 1e6, 'ConstraintTolerance', 1e-3, ...
+    'Algorithm','active-set');%, 'OutputFcn', term_fctn); % interior-point, sqp, trust-region-reflective, active-set
 options.MaxFunctionEvaluations = 2.5e5;
 options.MaxIterations = 1e3;
 
@@ -28,26 +31,27 @@ end
 function stop = customOutputFcn(x, optimValues, state, fval_tol, constr_tol)
     stop = false;
     % Check if the absolute function value is less than the threshold
-    if abs(optimValues.fval) < fval_tol && optimValues.constrviolation < constr_tol
+    if optimValues.constrviolation < constr_tol % && abs(optimValues.fval) < fval_tol
         stop = true;
         disp('Terminating: Absolute function value is below the threshold.');
     end
 end
 
 
-function val = cost_fctn(p, A1, b1)
+function val = cost_fctn(p, A0, b0)
 
 p = getCompVec(p);
-val = norm(abs(A1 * p) - b1);
+val = norm(abs(A0 * p) - b0);
 
 end
 
-function [c,ceq] = ineq_const(p, A2, b2)
+function [c,ceq] = ineq_const(p, A1, b1, A2, b2)
 
 p = getCompVec(p);
-c = abs(A2 * p) - b2;
 
-ceq = [];
+% ceq = [];
+ceq = abs(A1 * p) - b1;
+c = abs(A2 * p) - b2;
 
 end
 
