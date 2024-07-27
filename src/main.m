51291ee702fa9de8c8cd654b7f4c5d1f658abd80
Max Hasslberger <max.hasslberger@tum.de>
2024-07-26 17:10:35 -0700

    Option to change colormap

--- src/main.m
+++ src/main.m
@@ -91,7 +91,7 @@ else
     preplot_arg = preplot_arg + b_mask_plot;
     preplot_arg = preplot_arg / max(preplot_arg(:));
     plot_results(kgrid, [], preplot_arg, 'Plot Preview 2', mask2el, t1w_filename, plot_offset, grid_size, dx_factor1, false, [], 'slice', point_pos.slice, ...
-        'colorbar', false);
+        'colorbar', false, 'cmap', hot());
 end
 
 p_init = pinv(ip.A(init_ids, :)) * b_ip_des(init_ids, :);
@@ -144,19 +144,19 @@ if do_time_reversal
     % tr.b(~domain_ids) = 0.0;
     plot_results(kgrid, tr.p, tr.b, 'Time Reversal', mask2el, t1w_filename, plot_offset, grid_size, dx_factor1, save_results, current_datetime, 'slice', point_pos.slice);
     plot_results(kgrid, [], abs(tr.b) > max(b_des) / 2, 'Time Reversal', mask2el, t1w_filename, plot_offset, grid_size, dx_factor1, save_results, current_datetime, ...
-        'slice', point_pos.slice);
+        'slice', point_pos.slice, 'cmap', gray());
 end
 
 %% IP Results
 plot_results(kgrid, ip.p, ip.b, 'Inverse Problem', mask2el, t1w_filename, plot_offset, grid_size, dx_factor1, save_results, current_datetime, 'slice', point_pos.slice);
 plot_results(kgrid, [], abs(ip.b) > max(b_des) / 2, 'Inverse Problem', mask2el, t1w_filename, plot_offset, grid_size, dx_factor1, save_results, current_datetime, ...
-    'slice', point_pos.slice, 'colorbar', true); % plot mask with pressure above off-target limit
+    'slice', point_pos.slice, 'colorbar', true, 'cmap', gray()); % plot mask with pressure above off-target limit
 
 if do_ground_truth % For different resolution: Only supported in 3D at the moment
     plot_results(kgridP, [], ip.b_gt, 'Ground Truth', mask2el, t1w_filename, plot_offsetP, grid_sizeP, dx_factorP, save_results, ...
         current_datetime, 'slice', point_posP.slice);
     plot_results(kgridP, [], abs(ip.b_gt) > max(b_des) / 2, 'Ground Truth', mask2el, t1w_filename, plot_offsetP, grid_sizeP, dx_factorP, save_results, ...
-        current_datetime, 'slice', point_posP.slice, 'colorbar', true);
+        current_datetime, 'slice', point_posP.slice, 'colorbar', true, 'cmap', gray());
     
     if plot_dx_factor == 1
         err = abs(ip.b) - abs(ip.b_gt);
@@ -169,7 +169,7 @@ else
     plot_results(kgrid, ip.p_gt, ip.b_gt, 'Inverse Problem Comp', mask2el, t1w_filename, plot_offset, grid_size, dx_factor1, save_results, ...
         current_datetime, 'slice', point_pos.slice);
     plot_results(kgrid, [], abs(ip.b_gt) > max(b_des) / 2, 'Inverse Problem Comp', mask2el, t1w_filename, plot_offset, grid_size, dx_factor1, save_results, ...
-        current_datetime, 'slice', point_pos.slice, 'colorbar', true);
+        current_datetime, 'slice', point_pos.slice, 'colorbar', true, 'cmap', gray());
 end
 
 %% Metrics evaluation
