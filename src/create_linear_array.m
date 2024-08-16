a89e5048b54e79cc914657ec4a420f33603c0950
Max Hasslberger <max.hasslberger@tum.de>
2024-08-15 22:27:17 -0700

    2D transducer geometry fix

--- src/create_linear_array.m
+++ src/create_linear_array.m
@@ -2,9 +2,8 @@ function t_mask = create_linear_array(kgrid, tr_len, x_offset, y_offset, spacing
 
 if kgrid.dim == 2 % Create linear transducer array with approx. num_elements elements
 
-    t_mask = zeros(kgrid.Nx, kgrid.Ny);
     % Create a grid
-    [X, Y] = meshgrid(1:kgrid.Ny, 1:kgrid.Nx);
+    [Y, X] = meshgrid(1:kgrid.Ny, 1:kgrid.Nx);
 
     x_range = tr_len/2 * cos(phi/180*pi);
     y_range = tr_len/2 * sin(phi/180*pi);
@@ -26,25 +25,6 @@ if kgrid.dim == 2 % Create linear transducer array with approx. num_elements ele
     
     % Mask only the points within the bounding box and the line
     t_mask = lineMask & X >= min(y_ids) & X <= max(y_ids) & Y >= min(x_ids) & Y <= max(x_ids);
-
-
-%     y_spac_factor = ((sin(phi/180*pi) >= 0.0) - 0.5) * 2;
-    
-%     % Determine x and y locations along the line
-%     xvalues = round(linspace(x_offset - x_range, x_offset + x_range, num_elements));
-%     yvalues = round(linspace(y_offset - y_range, y_offset + y_range, num_elements));
-%     xvalues = round(x_offset - x_range):spacing:round(x_offset + x_range);
-%     yvalues = round(y_offset - y_range):round(y_spac_factor * spacing):round(y_offset + y_range);
-% 
-%     % Fit vector sizes to each other
-%     if length(xvalues) > length(yvalues)
-%         yvalues = fit_coordinates(length(xvalues), yvalues);
-%     elseif length(xvalues) < length(yvalues)
-%         xvalues = fit_coordinates(length(yvalues), xvalues);
-%     end
-%     
-%     % Replace the relevant values within the mask
-%     t_mask(sub2ind(size(t_mask), xvalues, yvalues)) = 1;
 else
     error("Function not supported in 3D")
 end
